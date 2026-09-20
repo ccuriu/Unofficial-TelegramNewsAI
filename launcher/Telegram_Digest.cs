2112b6a64d5f6890ee974ecdeef125d9e922deb3
Uriu <163174088+ccuriu@users.noreply.github.com>
2026-09-20 19:34:41 +0300

    Bump launcher metadata to 5.4.17 Testing

--- launcher/Telegram_Digest.cs
+++ launcher/Telegram_Digest.cs
@@ -6,10 +6,10 @@ using System.Reflection;
 using System.Windows.Forms;
 
 [assembly: AssemblyTitle("Unofficial TelegramNewsAI")]
-[assembly: AssemblyDescription("Запуск Unofficial TelegramNewsAI 5.4.16 Testing")]
+[assembly: AssemblyDescription("Запуск Unofficial TelegramNewsAI 5.4.17 Testing")]
 [assembly: AssemblyProduct("Unofficial TelegramNewsAI")]
-[assembly: AssemblyVersion("5.4.16.0")]
-[assembly: AssemblyFileVersion("5.4.16.0")]
+[assembly: AssemblyVersion("5.4.17.0")]
+[assembly: AssemblyFileVersion("5.4.17.0")]
 
 internal static class Program
 {
